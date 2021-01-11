e8141c1f543c9c2b0d3aca97f1e37712965bc062
Nobel-Justin <wenlongkxm@gmail.com>
2021-01-11 20:46:15 +0800

    update func

--- FuseSV.pm
+++ FuseSV.pm
@@ -6,36 +6,10 @@ use warnings;
 # basic
 use FuseSV::LoadOn;
 use FuseSV::RunFunc;
-# viral database
-use FuseSV::Virus_Type::PrepareExtendVirusRef;
-use FuseSV::Virus_Type::ConstructVirusDatabase;
-# virus type
-use FuseSV::Virus_Type::VirusTypeMain;
-use FuseSV::Virus_Type::CandReads;
-use FuseSV::Virus_Type::VirusInitAlign;
-use FuseSV::Virus_Type::ReconstrcutVirusVariants;
-use FuseSV::Virus_Type::PrepareForIntegDetection;
-# virus integration
-use FuseSV::Virus_Integ::DetectInteg;
-use FuseSV::Virus_Integ::Primers::GeneratePrimers;
-use FuseSV::Virus_Integ::MicroHomology::Find_MH;
-use FuseSV::Virus_Integ::Enrichment::IntegEnrichCheck;
-use FuseSV::Virus_Integ::RNA::FindIntegFlankASE;
-use FuseSV::Virus_Integ::RNA::MapVitgDNAvsRNA;
 # local map of virus integration
 use FuseSV::Virus_Integ::DrawSegCN::DrawSegCN;
 use FuseSV::Virus_Integ::LocalHaplotypeDetect::GetUnitCycle;
 use FuseSV::Virus_Integ::LocalHaplotypeDetect::UnitCycleToBioContig;
-use FuseSV::Virus_Integ::SegmentPhase::HostSegPhase;
-use FuseSV::Virus_Integ::LocalHaplotypeDetect::PairWiseAnchorLinkStat;
-use FuseSV::Virus_Integ::LocalHaplotypeCircos::DrawLocalMapCircos;
-# extension
-use FuseSV::Virus_Type::DepthOfViralGenome;
-use FuseSV::Virus_Type::DrawViralGenome;
-use FuseSV::Virus_Type::ConstructViralDNAtree;
-use FuseSV::Virus_Type::VirusMutLinkage;
-use FuseSV::Virus_Type::CreateMutatedVirusGenome;
-use FuseSV::Extension::GetCtrlVCFfromCase;
 
 #--- 
 1; ## tell the perl script the successful access of this module.
